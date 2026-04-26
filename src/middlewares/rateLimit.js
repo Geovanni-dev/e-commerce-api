a2c972396661c59f97f71b6918cd738d67ae5188
Geovani Rodrigues <geovanni3344@outlook.com>
2026-04-26 19:23:37 -0300

    fix: corrige exibição da mensagem de erro do rate limit no frontend
    fix: downgrade express para v4 para corrigir processo encerrando automaticamente

--- src/middlewares/rateLimit.js
+++ src/middlewares/rateLimit.js
@@ -1,24 +1,49 @@
 const rateLimit = require("express-rate-limit"); // importa o express-rate-limit
 
+const blockedIps = {};
+
 const globalLimiter = rateLimit({
         windowMs: 60 * 1000, //1 minuto
         max: 100, // limite de 100 requisicoes por minuto
         handler: (req, res) => {
-            res.status(429).json({ 
+        const ip = req.ip;
+        const now = Date.now();
+        
+        if (blockedIps[ip]  && now < blockedIps[ip]) {
+            const secondsLeft = Math.ceil((blockedIps[ip] - now) / 1000);
+            return res.status(429).json({ 
                 error: "Muitas tentativas frequeêntes, tente novamente mais tarde"
-    });
+            });
+        }
+        
+        blockedIps[ip] = now + 60 * 1000; // se o ip estiver bloqueado
+        
+            return res.status(429).json({ // envia resposta
+                error: "Muitas tentativas frequeêntes, tente novamente mais tarde"
+            });
         }
     });
-
 
     const limiter = rateLimit({
         windowMs: 60 * 1000, //1 minuto
         max: 5, // limite de 5 requisicoes por minuto
-        handler: (req, res) => {
-            res.status(429).json({ 
+        handler: (req, res) => { // funcao para lidar com o limite de requisicoes
+            const ip = req.ip;
+            const now = Date.now();
+            
+            if (blockedIps[ip]  && now < blockedIps[ip]) { // se o ip nao estiver bloqueado
+                const secondsLeft = Math.ceil((blockedIps[ip] - now) / 1000);
+                return res.status(429).json({ 
+                    error: "Muitas tentativas frequeêntes, tente novamente mais tarde"
+                });
+            }
+            
+            blockedIps[ip] = now + 60 * 1000; // se o ip estiver bloqueado
+            
+                return res.status(429).json({ // envia resposta
                 error: "Muitas tentativas frequeêntes, tente novamente mais tarde"
-    });
+            });
         }
     });
 
-   module.exports = { globalLimiter, limiter };
+    module.exports = { globalLimiter, limiter };
